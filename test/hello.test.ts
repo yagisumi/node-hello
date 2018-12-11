5f612ecc6d6967a123f4dffd5c5df21edfc85672
Yagi Sumiya <goatship4@yahoo.co.jp>
2018-12-12 00:39:56 +0900

    fix mock scope

--- test/hello.test.ts
+++ test/hello.test.ts
@@ -1,19 +1,21 @@
 import { hello } from "../lib/hello"
 
 describe("function hello()", () => {
-  const consoleLog = jest.spyOn(console, "log")
-
   test("hello()", () => {
+    const consoleLog = jest.spyOn(console, "log")
     hello()
     expect(console.log).toBeCalled()
     expect(consoleLog.mock.calls[0][0]).toBe("Hello!")
     consoleLog.mockReset()
+    consoleLog.mockRestore()
   })
 
   test("hello(name)", () => {
+    const consoleLog = jest.spyOn(console, "log")
     hello("Bob")
     expect(console.log).toBeCalled()
     expect(consoleLog.mock.calls[0][0]).toBe("Hello Bob!")
     consoleLog.mockReset()
+    consoleLog.mockRestore()
   })
 })
